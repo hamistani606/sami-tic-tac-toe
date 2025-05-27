13fd1f96c2bf27df1e4c245e3aedda58df1b7144
boerw-school <boerw@oregonstate.edu>
2025-05-27 16:24:20 -0700

    Update test1.py function call names

--- sami_ttt/behaviorbank/test1.py
+++ sami_ttt/behaviorbank/test1.py
@@ -1,9 +1,9 @@
-misty.move_head(0, 30, 0)
-misty.move_arm("left", 70) # choosing Misty's arm and its position
-misty.move_arm("right", -50)
+misty.MoveHead(0, 30, 0)
+misty.MoveArm("left", 70) # choosing Misty's arm and its position
+misty.MoveArm("right", -50)
 time.sleep(0.4)
-misty.move_arm("right", 0)
+misty.MoveArm("right", 0)
 time.sleep(0.4)
-misty.move_arm("right", -50)
+misty.MoveArm("right", -50)
 time.sleep(0.4)
-misty.move_arm("right", 0)
+misty.MoveArm("right", 0)
