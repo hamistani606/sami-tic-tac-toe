130df28f4aff6eb17374f5a0d079c44ab76e3998
kevickstrom <ore.sailor@gmail.com>
2025-06-04 13:51:06 -0700

    update animations

--- sami_ttt/behaviorbank/loss1.py
+++ sami_ttt/behaviorbank/loss1.py
@@ -1,11 +1,11 @@
 self.misty.MoveHead(26,0,0, None, 1.5) # Head tilts down (in shame)
 self.misty.MoveArms(90,90) # Arms go to sides
 time.sleep(0.5)
-self.misty.drive(-5,0,1000) # Slowly drives back
+self.misty.DriveTime(-5,0,1000) # Slowly drives back
 time.sleep(0.5)
 # All below slowly bring robot back to same position
 self.misty.MoveHead(0,0,0, None, 1.5)
 self.misty.MoveArms(0,0)
 time.sleep(0.75)
-self.misty.drive(5,0,1000)
+self.misty.DriveTime(5,0,1000)
 time.sleep(0.5)
--- sami_ttt/behaviorbank/win1.py
+++ sami_ttt/behaviorbank/win1.py
@@ -1,4 +1,4 @@
-self.misty.MoveHead(-30, 0, 0, Duration = 200) # Tilts head back in excitement
+self.misty.MoveHead(-30, 0, 0, duration = 200) # Tilts head back in excitement
 time.sleep(0.225)
 self.misty.MoveArms(-29, 0) # Waves hands in air, back and forth to up
 time.sleep(0.125)
@@ -10,13 +10,12 @@ self.misty.DriveTime(0, 40, 1000)
 time.sleep(0.325)
 self.misty.MoveArms(-29, 0)
 time.sleep(0.125)
-self.misty.DriveTime(0, -40, 1000)
-time.sleep(0.325)
+self.misty.DriveTime(0, -150, 2000)
+time.sleep(2)
 self.misty.MoveArms(0, -29)
-time.sleep(0.125)
-self.misty.DriveTime(0, 40, 1000)
-time.sleep(0.325)
-self.misty.MoveArms(0, 0, Duration = 200) # Zero entries move arms and head back to central position
+self.misty.DriveTime(0, 150, 2000)
+time.sleep(2)
+self.misty.MoveArms(0, 0, duration = 200) # Zero entries move arms and head back to central position
 time.sleep(0.225)
-self.misty.MoveHead(0, 0, 0, Duration = 200)
+self.misty.MoveHead(0, 0, 0, duration = 200)
 time.sleep(0.225)
