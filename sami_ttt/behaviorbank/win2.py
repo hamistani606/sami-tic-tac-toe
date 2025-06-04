84a14ccdc5824ac15b0cb2447d03926c66a2577f
John VW <vonworlj@oregonstate.edu>
2025-06-04 12:56:11 -0700

    Win2 change

--- sami_ttt/behaviorbank/win2.py
+++ sami_ttt/behaviorbank/win2.py
@@ -1,10 +1,16 @@
 # Movement meant to mimic hand shaking
 self.misty.MoveHead(0, 0, 0) # Centers head
 self.misty.DriveTime(10, 0, 500, 0) # Slowly moves forward
-time.sleep(0.25)
+time.sleep(0.5)
 # Shakes arm up and down to mimic hadn
-self.misty.MoveArm('right', 0, 40)
-self.misty.MoveArm('right', 30, 50)
 self.misty.MoveArm('right', 0, 50)
+time.sleep(.125)
 self.misty.MoveArm('right', 30, 50)
+time.sleep(.125)
 self.misty.MoveArm('right', 0, 50)
+time.sleep(.125)
+self.misty.MoveArm('right', 30, 50)
+time.sleep(.125)
+self.misty.MoveArm('right', 0, 50)
+time.sleep(.125)
+self.misty.DriveTime(-10, 0, 500, 0) # Slowly moves back
