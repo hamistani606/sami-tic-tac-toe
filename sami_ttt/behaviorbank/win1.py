0c8abd894b4957948cea77883d8fe5f33ccb2323
John VW <vonworlj@oregonstate.edu>
2025-06-04 12:53:48 -0700

    Win1 Modification

--- sami_ttt/behaviorbank/win1.py
+++ sami_ttt/behaviorbank/win1.py
@@ -1,14 +1,13 @@
-self.misty.MoveHead(0, 0, 0)
 self.misty.MoveArms(-29, 0) # Waves hands in air, back and forth to up
-time.sleep(0.25)
+time.sleep(0.125)
 self.misty.drive(0, -20) # Turns left, then right slightly. Intended to mimic excitement
 self.misty.MoveArms(0, -29)
-time.sleep(0.25)
+time.sleep(0.125)
 self.misty.drive(0, 20)
 self.misty.MoveArms(-29, 0)
-time.sleep(0.25)
+time.sleep(0.125)
 self.misty.drive(0, -20)
 self.misty.MoveArms(0, -29)
-time.sleep(0.25)
+time.sleep(0.125)
 self.misty.drive(0, 20)
 self.misty.MoveArms(0, 0)
