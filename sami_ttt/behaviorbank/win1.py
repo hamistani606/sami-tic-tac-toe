8c889277725e55e20d8ec0fb92be12497ce40166
John VW <vonworlj@oregonstate.edu>
2025-06-04 13:25:45 -0700

    Fixed mistakes

--- sami_ttt/behaviorbank/win1.py
+++ sami_ttt/behaviorbank/win1.py
@@ -2,19 +2,19 @@ self.misty.MoveHead(-30, 0, 0, Duration = 200) # Tilts head back in excitement
 time.sleep(0.225)
 self.misty.MoveArms(-29, 0) # Waves hands in air, back and forth to up
 time.sleep(0.125)
-self.misty.DriveTime(0, -40, 300) # Turns left, then right slightly. Intended to mimic excitement
+self.misty.DriveTime(0, -40, 1000) # Turns left, then right slightly. Intended to mimic excitement
 time.sleep(0.325)
 self.misty.MoveArms(0, -29)
 time.sleep(0.125)
-self.misty.DriveTime(0, 40, 300)
+self.misty.DriveTime(0, 40, 1000)
 time.sleep(0.325)
 self.misty.MoveArms(-29, 0)
 time.sleep(0.125)
-self.misty.DriveTime(0, -40, 300)
+self.misty.DriveTime(0, -40, 1000)
 time.sleep(0.325)
 self.misty.MoveArms(0, -29)
 time.sleep(0.125)
-self.misty.DriveTime(0, 40, 300)
+self.misty.DriveTime(0, 40, 1000)
 time.sleep(0.325)
 self.misty.MoveArms(0, 0, Duration = 200) # Zero entries move arms and head back to central position
 time.sleep(0.225)
